ec1365d4437c9b7b4973bb13520f9d4ad734a67b
Pavel Sekishov <pasha-sveta@yandex.ru>
2021-05-19 15:04:16 +0300

    project_cds
    Create main files

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+    $('.slick').slick({
+        dots: false,
+        infinite: true,
+        speed: 1500,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        pauseOnHover: true,
+        fade: true,
+        cssEase: 'linear'
+    });
+    
+});
